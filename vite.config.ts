9ff3ec7f50bdb056edf1e31141db4eab302ad2c8
Azhaqcom <arnaudleg.dev@gmail.com>
2025-08-31 23:46:01 +0200

    gh

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+const base_path = process.env.VITE_APP_BASE_PATH || '';
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+   base: base_path,
 })
